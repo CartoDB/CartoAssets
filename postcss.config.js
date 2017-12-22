13118b78dba7d7e949e1055fb25f56275ec7fab8
rubenmoya <ruben@carto.com>
2017-12-22 13:35:13 +0100

    Setup PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+const POSTCSS = {
+  'input': 'src/entry.css',
+  'output': 'dist/carto_assets.css',
+  'use': [
+    'postcss-import',
+    'postcss-cssnext'
+    // 'postcss-browser-reporter',
+    // 'postcss-reporter'
+  ],
+  'postcss-import': {
+    path: ['src']
+  },
+  'cssnano': {
+    safe: true,
+    autoprefixer: false //handled by postcss-cssnext
+  },
+  'local-plugins': true,
+  'map': 'file'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  POSTCSS.use.push('cssnano'); // add cssnano in production
+  POSTCSS.map = false; //sourcemaps to external file
+}
+
+module.exports = POSTCSS;
